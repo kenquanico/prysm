d96c10252e1a21748160a7cdf9eb0310a457bf6f
kenquan <nekquanico@gmail.com>
2026-05-24 12:21:31 +0800

    Initial Commit

---
+++ Prysm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Prysm
+//
+//  Created by Ken Aldrey Quanico on 5/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Prysm/PrysmApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrysmApp.swift
+//  Prysm
+//
+//  Created by Ken Aldrey Quanico on 5/24/26.
+//
+
+import SwiftUI
+
+@main
+struct PrysmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
